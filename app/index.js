03b58c0881eba445b224689c22cb35ce9b777931
ethyi <ethyi@proton.me>
2022-06-19 13:02:45 -0700

    rankings styled

--- app/index.js
+++ app/index.js
@@ -10,7 +10,7 @@ class App extends React.Component {
   render() {
     const name = "Ethan";
     return (
-      <div>
+      <div className="container">
         <Popular />
       </div>
     );
